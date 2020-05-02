5fd2b9a071113d3bead8b45f6df961e0f65d5eaf
DenFade <rurchi1206@gmail.com>
2020-05-03 01:23:12 +0900

    change all pre connection system

--- global_modules/gd/dfconnection.js
+++ global_modules/gd/dfconnection.js
@@ -1,52 +1,45 @@
-module.exports = {
-    GET: function(url, header, timeout, cookie, ignoreType, toText, callback){
-        var result;
-        try{
-            result = org.jsoup.Jsoup.connect(url).timeout(timeout);
-            for(i in header){
-                result = result.header(i, header[i]);
-            }
-            if(ignoreType){
-               result = result.ignoreContentType(true);
-            }
-            result = result.get();
-            callback(toText ? result.text() : result, null);
-        } catch(e) {
-            callback(null, {
-                error: e,
-                url: url,
-                header: header,
-                timeout: timeout,
-                cookie: cookie,
-                ignoreType: ignoreType,
-                toText: toText
-            });
+function Connection(){
+    function Connection(url, header, body, timeout, cookie, method, ignoreType, toText, callback){
+        this.url = url;
+        this.header = header;
+        this.cookie = cookie;
+        this.ignoreType = ignoreType;
+        this.toText = toText;
+        this.callback = callback;
+        this.result = org.jsoup.Jsoup.connect(url).timeout(timeout);
+        for(i in header){
+            this.result = this.result.header(i, header[i]);
         }
-    },
-    POST: function(url, header, body, timeout, cookie, ignoreType, toText, callback){
-        var result;
-        try{
-            result = org.jsoup.Jsoup.connect(url).timeout(timeout);
-            for(i in header){
-                result = result.header(i, header[i]);
-            }
-            result = result.requestBody(body);
-            if(ignoreType){
-               result = result.ignoreContentType(true);
-            }
-            result = result.post();
-            callback(toText ? result.text() : result, null);
-        } catch(e) {
-            callback(null, {
-                error: e,
-                url: url,
-                header: header,
-                body: body,
-                timeout: timeout,
-                cookie: cookie,
-                ignoreType: ignoreType,
-                toText: toText
-            });
+        for(c in cookie){
+            this.result = this.result.cookie(c, cookie[c]);
         }
+        if(ignoreType){
+            this.result = this.result.ignoreContentType(true);
+        }
+        if(body){
+            this.result = this.result.requestBody(body);
+        }
+        this.result = this.result.method(method);
+    }
+
+    Connection.GET = function(url, header, timeout, cookie, ignoreType, toText, callback){
+        let con = new Connection(url, header, null, timeout, cookie, org.jsoup.Connection.Method.GET, ignoreType, toText, callback);
+        return con;
+    }
+
+    Connection.POST = function(url, header, body, timeout, cookie, ignoreType, toText, callback){
+        let con = new Connection(url, header, body, timeout, cookie, org.jsoup.Connection.Method.POST, ignoreType, toText, callback);
+        return con;
+    }
+
+    Connection.prototype.block = function(){
+        var res;
+        try{
+            res = this.result.execute();
+        } catch(e){
+            this.callback(null, e);
+            return;
+        }
+        return this.callback(res, null);
     }
 }
